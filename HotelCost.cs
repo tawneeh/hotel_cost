3fe52d1b3c078ad0cea9c89b6a0fbd4f312e90b5
Tawnee Harris <tawneeh@icloud.com>
2020-12-06 09:41:28 -0800

    add initial C# code

---
+++ HotelCost.cs
@@ -0,0 +1,25 @@
+using System;
+
+class HotelCost
+{
+    static void Main()
+    {
+      Console.WriteLine("In what season are you booking a stay?");
+      string season = Console.ReadLine();
+
+      Console.WriteLine("On the weekend or a weeknight?");
+      string dayOfWeek = Console.ReadLine();
+
+      bool summer = season == "summer";
+      bool weekend = dayOfWeek == "weekend";
+
+      if (summer && weekend)
+      {
+        Console.WriteLine("Your stay is going to be pretty expensive! It's both peak travel season AND the weekend.");
+      }
+      else
+      {
+        Console.WriteLine("Your stay might be expensive, but it could be worse!");
+      }
+    }
+}
